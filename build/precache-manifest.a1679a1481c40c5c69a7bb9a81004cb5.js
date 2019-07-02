34446dd584144ff003d00f6b97fb71db9cc65f3d
Mikko Pelkonen <m.a.pelkonen2@gmail.com>
2019-07-02 11:02:21 +0300

    frontend

---
+++ build/precache-manifest.a1679a1481c40c5c69a7bb9a81004cb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970937e85581ed999eb8801fe00c3ab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb47d3f5d7de3234ed8e",
+    "url": "/static/css/main.90418fa9.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "bb47d3f5d7de3234ed8e",
+    "url": "/static/js/main.ed73e02b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/precache-manifest.a6db35e882a675ba683bc47575d9989e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1e7b2138d3cb03761ec7efcee3df2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d7728ff864507d4dfc",
+    "url": "/static/css/main.90418fa9.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "62d7728ff864507d4dfc",
+    "url": "/static/js/main.47666e5c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
